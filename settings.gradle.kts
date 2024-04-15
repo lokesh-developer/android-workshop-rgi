264311b18484c919c8c4a8e64fa44a7cb31d70f1
Diligent Coder <kabral1999@gmail.com>
2024-04-15 09:04:57 +0000

    java app :)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,18 @@
  *
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.6/userguide/multi_project_builds.html in the Gradle documentation.
+ * This project uses @Incubating APIs which are subject to change.
  */
 
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
 rootProject.name = "android-workshop-rgi"
+include("app", "list", "utilities")
